f7a58d0a81153a2f0faf1b4e0fc5150fa7df0d0d
Daniel Wutke <dwu.public@gmail.com>
2019-01-01 16:08:47 +0100

    Initial import

---
+++ main.js
@@ -0,0 +1,34 @@
+/* Configuration */
+const port = 3000;
+/* Configuration End */
+
+const express = require('express');
+
+const app = express();
+app.use(express.static('static'));
+
+/* Main */
+
+// Common APIs
+app.get('/api/products', (req, res) => {
+    res.send(200, {
+        items: {
+            1: {
+                name: "Product 1",
+                price: 100
+            },
+            2: {
+                name: "Product 2",
+                price: 200
+            },
+            3: {
+                name: "Product 3",
+                price: 300
+            }
+        }
+    });
+});
+
+// Scenario 1 - IFrames
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
