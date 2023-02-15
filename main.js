04d992f83775a3c7546232416a24eb4d357b8ad3
Daniel Wutke <dwu.public@gmail.com>
2023-02-15 20:47:00 +0100

    Add AJAX example

--- main.js
+++ main.js
@@ -8,25 +8,31 @@ const app = express();
 app.use(express.static('static'));
 
 /* Main */
+var data = {
+    1: {
+        name: "Product 1",
+        description: "This is the awesome product 1",
+        price: 100
+    },
+    2: {
+        name: "Product 2",
+        description: "This is the even more awesome product 2",
+        price: 200
+    },
+    3: {
+        name: "Product 3",
+        description: "This is the most awesome product 3",
+        price: 300
+    }
+};
 
 // Common APIs
 app.get('/api/products', (req, res) => {
-    res.status(200).send({
-        items: {
-            1: {
-                name: "Product 1",
-                price: 100
-            },
-            2: {
-                name: "Product 2",
-                price: 200
-            },
-            3: {
-                name: "Product 3",
-                price: 300
-            }
-        }
-    });
+    res.status(200).send({ items: data });
+});
+
+app.get('/api/product-info/:id', (req, res) => {
+    res.status(200).send(`<h2>Product info for product ${req.params.id}</h2>${data[req.params.id].description}`);
 });
 
 // Scenario 1 - IFrames
---
+++ static/03-ajax/product-list.js
@@ -0,0 +1,49 @@
+class ProductList extends HTMLElement {
+    connectedCallback() {
+        this.refresh = this.refresh.bind(this);
+        this.products = {};
+
+        this.log('connected');
+        this.refresh();
+    }
+    refresh() {
+        this.log('refresh');
+        axios.get('/api/products').then((response) => {
+            this.products = response.data.items;
+            this.render();
+        });
+    }
+    render() {
+        this.log('render');
+
+        this.innerHTML = `
+            <div id="product-list">
+                <h2>Product List</h2>
+                <ul>
+                    ${Object.keys(this.products).map(function(key) {
+                        return `<li id="product-${key}">${this.products[key].name} (${this.products[key].price}€) <button id="addToCart-${key}">Add to cart</button></li>`;
+                    }, this).join('')}
+                </ul>
+            </div>`;
+
+        // Attach handlers
+        Object.keys(this.products).map(function(key) {
+            document.getElementById(`product-${key}`).addEventListener('mouseenter', function () {
+                this.mouseEnter(key);
+            }.bind(this));
+            // no handler for button click in this example
+        }, this);
+    }
+    mouseEnter(productId) {
+        const event = new CustomEvent('hoverProduct', { 'detail': { 'productId' : productId } });
+        this.dispatchEvent(event);
+    }
+    disconnectedCallback() {
+        this.log('disconnected');
+    }
+    log(...args) {
+        console.log('product-list', ...args);
+    }
+}
+
+window.customElements.define('product-list', ProductList);
