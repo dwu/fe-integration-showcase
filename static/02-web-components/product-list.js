41aa6799679bcf5cade59d925da44670d698bc38
Daniel Wutke <dwu.public@gmail.com>
2023-02-15 20:46:51 +0100

    Simplify web components implementation

--- static/02-web-components/product-list.js
+++ static/02-web-components/product-list.js
@@ -1,46 +1,48 @@
-(function fragments() {
+class ProductList extends HTMLElement {
+    connectedCallback() {
+        this.refresh = this.refresh.bind(this);
+        this.products = {};
 
-    class ProductList extends HTMLElement {
-        connectedCallback() {
-            this.refresh = this.refresh.bind(this);
-            this.products = {};
-
-            this.log('connected');
-            this.refresh();
-        }
-        refresh() {
-            this.log('refresh');
-            axios.get('/api/products').then((response) => {
-                this.products = response.data.items;
-                this.render();
-            });
-        }
-        render() {
-            this.log('render');
-
-            this.innerHTML = `
-                <div id="product-list">
-                    <h2>Product List</h2>
-                    <ul>
-                        ${Object.keys(this.products).map(function(key) {
-                            return `<li>${this.products[key].name} (${this.products[key].price}€) <button onClick="productList_addToCart(${key})">Add to cart</button></li>`;
-                        }, this).join('')}
-                    </ul>
-                </div>`;
-        }
-        disconnectedCallback() {
-            this.log('disconnected');
-        }
-        log(...args) {
-            console.log('product-list', ...args);
-        }
+        this.log('connected');
+        this.refresh();
     }
+    refresh() {
+        this.log('refresh');
+        axios.get('/api/products').then((response) => {
+            this.products = response.data.items;
+            this.render();
+        });
+    }
+    render() {
+        this.log('render');
 
-    window.customElements.define('product-list', ProductList);
+        this.innerHTML = `
+            <div id="product-list">
+                <h2>Product List</h2>
+                <ul>
+                    ${Object.keys(this.products).map(function(key) {
+                        return `<li>${this.products[key].name} (${this.products[key].price}€) <button id="addToCart-${key}">Add to cart</button></li>`;
+                    }, this).join('')}
+                </ul>
+            </div>`;
 
-}());
-
-function productList_addToCart(productId) {
-    const event = new CustomEvent('ProductList-addToCart', { 'detail': { 'productId' : productId } });
-    window.dispatchEvent(event);
+        // Attach handlers
+        Object.keys(this.products).map(function(key) {
+            document.getElementById(`addToCart-${key}`).addEventListener('click', function () {
+                this.addToCart(key);
+            }.bind(this));
+        }, this);
+    }
+    addToCart(productId) {
+        const event = new CustomEvent('addToCart', { 'detail': { 'productId' : productId } });
+        this.dispatchEvent(event);
+    }
+    disconnectedCallback() {
+        this.log('disconnected');
+    }
+    log(...args) {
+        console.log('product-list', ...args);
+    }
 }
+
+window.customElements.define('product-list', ProductList);
--- static/02-web-components/shopping-cart.js
+++ static/02-web-components/shopping-cart.js
@@ -1,85 +1,85 @@
-(function fragments() {
+class ShoppingCart extends HTMLElement {
+    connectedCallback() {
+        this.refresh = this.refresh.bind(this);
+        this.products = {};
+        this.cart = [];
+        this.total = 0;
 
-    class ShoppingCart extends HTMLElement {
-        connectedCallback() {
-            this.refresh = this.refresh.bind(this);
-            this.products = {};
-            this.cart = [];
-            this.total = 0;
+        this.log('connected');
+        this.refresh();
 
-            this.log('connected');
-            this.refresh();
-
-            handleEvent(window, 'ProductList-addToCart', function (event) {
-                this.addToCart(event.productId);
-            }.bind(this));
-        }
-        refresh() {
-            this.log('refresh');
-            axios.get('/api/products').then((response) => {
-                this.products = response.data.items;
-                this.render();
-            });
-        }
-        render() {
-            this.log('render');
-
-            // calculate total
-            this.total = 0;
-            this.cart.forEach(function (item) {
-                this.total += this.products[item].price;
-            }, this);
-
-            // render component
-            this.innerHTML = `
-                <div id="shopping-cart">
-                    <h2>Shopping Cart</h2>
-                    <ul>
-                        ${this.cart.map(function (key, pos) {
-                return `<li>${this.products[key].name} (${this.products[key].price}€) <button id="removeFromCart-${pos}">X</button></li>`;
-            }, this).join('')}
-                    </ul>
-                    Total: ${this.total} <button id="buy">Buy</button>
-                    <button id="clear">Clear cart</button>
-
-                </div>`;
-
-            // attach handlers
-            this.cart.forEach(function (key, pos) {
-                document.getElementById(`removeFromCart-${pos}`).addEventListener('click', function () {
-                    this.removeFromCart(pos);
-                }.bind(this));
-            }, this);
-
-            document.getElementById("buy").addEventListener('click', function () {
-                if (this.total > 0) {
-                    alert('Thanks for your ' + this.total + '€. :)');
-                } else {
-                    alert('Thanks for nothing!');
-                }
-            }.bind(this));
-
-            document.getElementById("clear").addEventListener('click', function () {
-                this.cart = [];
-                this.render();
-            }.bind(this));
-        }
-        disconnectedCallback() {
-            this.log('disconnected');
-        }
-        log(...args) {
-            console.log('shopping-cart', ...args);
-        }
-        addToCart(productId) {
-            this.cart.push(productId);
-            this.render();
-        }
-        removeFromCart(pos) {
-            this.cart.splice(pos, 1);
-            this.render();
-        }
+        handleEvent(this, 'addToCart', function (event) {
+            this.addToCart(event.productId);
+        }.bind(this));
     }
+    refresh() {
+        this.log('refresh');
+        axios.get('/api/products').then((response) => {
+            this.products = response.data.items;
+            this.render();
+        });
+    }
+    render() {
+        this.log('render');
 
-    window.customElements.define('shopping-cart', ShoppingCart);
+        // calculate total
+        this.total = 0;
+        this.cart.forEach(function (item) {
+            this.total += this.products[item].price;
+        }, this);
 
-}());
+        // render component
+        this.innerHTML = `
+            <div id="shopping-cart">
+                <h2>Shopping Cart</h2>
+                <ul>
+                    ${this.cart.map(function (key, pos) {
+            return `<li>${this.products[key].name} (${this.products[key].price}€) <button id="removeFromCart-${pos}">X</button></li>`;
+        }, this).join('')}
+                </ul>
+                Total: ${this.total} <button id="buy">Buy</button>
+                <button id="clear">Clear cart</button>
+
+            </div>`;
+
+        // attach handlers
+        this.cart.forEach(function (key, pos) {
+            document.getElementById(`removeFromCart-${pos}`).addEventListener('click', function () {
+                this.removeFromCart(pos);
+            }.bind(this));
+        }, this);
+
+        document.getElementById("buy").addEventListener('click', function () {
+            if (this.total > 0) {
+                alert('Thanks for your ' + this.total + '€. :)');
+            } else {
+                alert('Thanks for nothing!');
+            }
+            this.clearCart();
+        }.bind(this));
+
+        document.getElementById("clear").addEventListener('click', function () {
+            this.clearCart();
+        }.bind(this));
+    }
+    disconnectedCallback() {
+        this.log('disconnected');
+    }
+    log(...args) {
+        console.log('shopping-cart', ...args);
+    }
+    addToCart(productId) {
+        this.cart.push(productId);
+        this.render();
+    }
+    removeFromCart(pos) {
+        this.cart.splice(pos, 1);
+        this.render();
+    }
+    clearCart() {
+        this.cart = [];
+        this.render();        
+    }
+}
+
+window.customElements.define('shopping-cart', ShoppingCart);
